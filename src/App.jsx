3df24a491e7b37dbce4114a3a9efe3e2a989585d
Matti Luukkainen <mluukkai@iki.fi>
2023-08-14 14:15:00 +0300

    part2-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
+import Footer from './components/Footer'
 import noteService from './services/notes'
 
 const App = () => {
@@ -83,7 +84,8 @@ const App = () => {
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form> 
+      </form>
+      <Footer />
     </div>
   )
 }
