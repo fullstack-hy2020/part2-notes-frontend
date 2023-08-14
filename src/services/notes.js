0d1504ab2bc0173b7ab3961ded1657aadfd3985f
Matti Luukkainen <mluukkai@iki.fi>
2023-08-14 13:24:03 +0300

    part2-6

---
+++ src/services/notes.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+/*
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}*/
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { 
+  getAll, create, update 
+}
