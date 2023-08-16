3fac3afa503ede22eaabfe8f5e7a7cb887f53b1c
Matti Luukkainen <mluukkai@iki.fi>
2023-08-16 14:44:28 +0300

    part5-3

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -66,6 +75,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,13 +1,23 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
 const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.post(baseUrl, newObject, config)
   return request.then(response => response.data)
 }
 
@@ -17,5 +27,5 @@ const update = (id, newObject) => {
 }
 
 export default { 
-  getAll, create, update 
+  getAll, create, update, setToken
 }
