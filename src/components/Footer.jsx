c9dd6931d2ca7ff36333e6ad0f3ec633f1cf0a9b
Matti Luukkainen <mluukkai@iki.fi>
2024-03-03 15:54:13 +0200

    part5-9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div style={footerStyle}>
       <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
     </div>
   )
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -15,6 +15,7 @@ const LoginForm = ({
         <div>
           username
           <input
+            data-testid='username'
             value={username}
             onChange={handleUsernameChange}
           />
@@ -22,6 +23,7 @@ const LoginForm = ({
         <div>
           password
           <input
+            data-testid='password'
             type="password"
             value={password}
             onChange={handlePasswordChange}
