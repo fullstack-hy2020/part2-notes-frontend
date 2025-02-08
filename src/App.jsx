1ee3623bb90b8109e5766fb045ffeacae07459c6
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-02-08 10:40:35 +0200

    part2-5

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,22 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
     axios.get('http://localhost:3001/notes').then((response) => {
-      console.log('promise fulfilled')
       setNotes(response.data)
     })
   }, [])
-  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: String(notes.length + 1),
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
