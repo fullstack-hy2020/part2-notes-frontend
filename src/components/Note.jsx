6a15e8d9cb0cf5a3fe46f33414f9ce02edb51762
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-02-10 20:08:09 +0200

    part2-7

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important ? 'make not important' : 'make important'
 
   return (
-    <li>
+    <li className="note">
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>
---
+++ src/components/Notification.jsx
@@ -0,0 +1,9 @@
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return <div className="error">{message}</div>
+}
+
+export default Notification
