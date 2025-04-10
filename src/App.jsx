b81f82c03c7c542b2a009db766f4db264f241e7a
Nithish699 <nithish699734@gmail.com>
2025-04-10 14:47:45 +0530

    Add state management for notes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
 const App = ({ notes }) => {
+  const [notess, setNotes] = useState(notes)
+  console.log(notess);
+  
   return (
     <div>
       <h1>Notes</h1>
