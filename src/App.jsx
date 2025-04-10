b319aa1f42ebdd84b47bda3b4176a7b1362507fc
Nithish699 <nithish699734@gmail.com>
2025-04-10 16:21:56 +0530

    Refactor App component to manage notes state and improve note addition functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import Note from './components/Note'
 import { useState } from 'react'
 
-const App = ({ notes }) => {
-  const [notess, setNotes] = useState(notes)
-  console.log(notess);
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('') 
+  const [showAll, setShowAll] = useState(true)
   
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
+const handleNoteChange = (event) => {
+  setNewNote(event.target.value)
+}
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    console.log(notes);
+    
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
-        ))}
+        )}
       </ul>
+
+      <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange}/>
+      <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
