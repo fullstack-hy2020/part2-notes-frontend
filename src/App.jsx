f6512c9f9e0e1d6d2c1df8467d6ed39b7971c466
Nithish699 <nithish699734@gmail.com>
2025-04-12 20:09:00 +0530

    feat: integrate json-server for notes management and update App component
    - Added json-server as a dependency and configured a server script in package.json.
    - Modified App component to fetch notes from the json-server on mount using axios.
    - Updated the addNote function to post new notes to the json-server.
    - Removed hardcoded notes from main.jsx and adjusted rendering of the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  
+  console.log('render', notes.length, 'notes')
   
   const notesToShow = showAll
   ? notes
@@ -14,19 +28,22 @@ const handleNoteChange = (event) => {
   setNewNote(event.target.value)
 }
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-    console.log(notes);
-    
+const addNote = event => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    important: Math.random() < 0.5,
   }
 
+
+  axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      console.log(response)
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+}
   return (
     <div>
       <h1>Notes</h1>
