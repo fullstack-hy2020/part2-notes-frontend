9c2a3065d3a9d95a6cad9edb685baf6526b5c95d
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-08-20 17:59:26 +0300

    Add id for login button as Cypress tests need it

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -30,7 +30,9 @@ const LoginForm = ({
             />
           </label>
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">
+          login
+        </button>
       </form>
     </div>
   )
