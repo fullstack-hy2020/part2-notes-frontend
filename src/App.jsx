427f1bb1b4484ed2dc67a87f6437ffdee36aac8d
dibyarupdeb <dibyarup.deb.ai@gmail.com>
2026-02-03 21:03:55 +0530

    Changed the type of the 'id' field in the 'noteObject' object from String type to Number type.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: String(notes.length + 1),
+      id: notes.length + 1,
     }
 
     setNotes(notes.concat(noteObject))
