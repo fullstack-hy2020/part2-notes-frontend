a54c613c62b12804520819998b2d9fc102cba0aa
Shuyu Tang <83319343+usun-111@users.noreply.github.com>
2026-02-06 16:07:43 +0800

    Refactor App component to manage notes with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import Note from './components/Note'
+import {useState} from 'react'
 
 const App = ({ notes }) => {
+  const {notes, setNotes} = useState(notes)
+  const {newNote, setNewNote} = useState("a new note...")
+  const {showAll, setShowAll} = useState(true) 
+
+  const notesToShow = showAll? notes : notes.filter(note => note.important) 
+  
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value))
+  }
+   
+   const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1), 
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote} 
+          onChange={handleNoteChange} 
+        />
+        <button type="submit">save</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll? 'all' : 'important'} 
+      </button>
     </div>
   )
 }
