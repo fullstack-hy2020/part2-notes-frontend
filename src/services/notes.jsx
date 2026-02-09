e12893b9fde78f0ce4e657420731562327b6dbd7
Shuyu Tang <shuyuTang@student.unsw.edu.au>
2026-02-10 01:32:52 +0800

    Set BaseUrl

--- src/services/notes.jsx
+++ src/services/notes.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 // const getAll = () => {
 //   const request = axios.get(baseUrl)
